6a73179a69553c61db57dc1e82005c49d9e6537a
RanolP <me@ranolp.dev>
2023-12-10 02:19:42 +0900

    Update batch.bat

--- setup-scripts/batch.bat
+++ setup-scripts/batch.bat
@@ -2,3 +2,8 @@
 cls
 echo You are now installing RanolP's dotfiles...
 echo Environment : Windows + Batch
+
+echo Installing essential packages...
+:: Git for Windows
+winget install --exact --id Git.Git
+winget install --exact --id Nushell.Nushell

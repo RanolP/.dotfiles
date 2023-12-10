35027d70f2a103ab153d3b6bde8935a1825c3087
RanolP <me@ranolp.dev>
2023-12-10 16:40:27 +0900

    Update batch.bat

--- setup-scripts/batch.bat
+++ setup-scripts/batch.bat
@@ -17,10 +17,5 @@ echo y | winget install --exact --id uutils.coreutils
 
 :: refresh "Path" env var
 echo Refreshing paths for getting "git" and "nu" executable...
-echo @echo off >> %Temp%\refresh.bzt
-echo for /f "usebackq skip=2 tokens=2,*" %%a in (`%WinDir%\System32\Reg QUERY "HKLM\System\CurrentControlSet\Control\Session Manager\Environment" /v Path`) do ( >> %Temp%\refresh.bat
-echo  for /f "usebackq skip=2 tokens=2,*" %%c in (`%WinDir%\System32\Reg QUERY "HKCU\Environment" /v Path`) do ( >> %Temp%\refresh.bat
-echo    set Path=%%b;%%d >> %Temp%\refresh.bat
-echo  ) >> %Temp%\refresh.bat
-echo ) >> %Temp%\refresh.bat
-call %Temp%\refresh.bat
+curl -L dotfiles.ranolp.dev/utils/windows/refresh-path.bat > %Temp%\refresh-path.bat
+call %Temp%\refresh-path.bat

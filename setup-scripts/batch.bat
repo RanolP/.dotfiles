e21fcf0eff43e876f310ef58b42eeabf89acd20c
RanolP <me@ranolp.dev>
2023-12-10 15:06:07 +0900

    Update batch.bat

--- setup-scripts/batch.bat
+++ setup-scripts/batch.bat
@@ -10,3 +10,11 @@ echo y | winget install --exact --id Git.Git
 :: nushell
 echo $ winget install --exact --id Nushell.Nushell
 echo y | winget install --exact --id Nushell.Nushell
+
+:: refresh "Path" env var
+echo Refreshing paths for getting "git" and "nu" executable...
+for /f "usebackq skip=2 tokens=2,*" %%a in (`%WinDir%\System32\Reg QUERY "HKLM\System\CurrentControlSet\Control\Session Manager\Environment" /v Path`) do (
+  for /f "usebackq skip=2 tokens=2,*" %%c in (`%WinDir%\System32\Reg QUERY "HKCU\Environment" /v Path`) do (
+    set Path=%%b;%%d
+  )
+)

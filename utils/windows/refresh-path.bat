368624c838ac6376251733ee6ea90fa7e9642ffa
RanolP <me@ranolp.dev>
2023-12-10 16:55:55 +0900

    Update refresh-path.bat

--- utils/windows/refresh-path.bat
+++ utils/windows/refresh-path.bat
@@ -1,6 +1,7 @@
 @echo off
 for /f "usebackq skip=2 tokens=2,*" %%a in (`%WinDir%\System32\Reg QUERY "HKLM\System\CurrentControlSet\Control\Session Manager\Environment" /v Path`) do (
   for /f "usebackq skip=2 tokens=2,*" %%c in (`%WinDir%\System32\Reg QUERY "HKCU\Environment" /v Path`) do (
-    set Path=%%b;%%d
+    call echo %%b;%%d > %Temp%\env.tmp
+    set /p Path=<%Temp%\env.tmp
   )
 )

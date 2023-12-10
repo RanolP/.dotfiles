b1a8f483f921f154d54ff6f19bb0db807291f965
RanolP <me@ranolp.dev>
2023-12-10 16:20:39 +0900

    Update batch.bat

--- setup-scripts/batch.bat
+++ setup-scripts/batch.bat
@@ -4,12 +4,16 @@ echo You are now installing RanolP's dotfiles...
 echo Environment : Windows + Batch
 
 echo Installing essential packages...
+echo You may interfered with several popups.
 :: Git for Windows
 echo $ winget install --exact --id Git.Git
 echo y | winget install --exact --id Git.Git
 :: nushell
 echo $ winget install --exact --id Nushell.Nushell
 echo y | winget install --exact --id Nushell.Nushell
+:: uutils coreutils
+echo $ winget install --exact --id uutils.coreutils
+echo y | winget install --exact --id uutils.coreutils
 
 :: refresh "Path" env var
 echo Refreshing paths for getting "git" and "nu" executable...

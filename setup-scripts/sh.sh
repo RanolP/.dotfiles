cd2c7ba872707d36b86417f49bf50097d4a8d021
RanolP <me@ranolp.dev>
2024-01-06 03:37:54 +0900

    arch now

--- setup-scripts/sh.sh
+++ setup-scripts/sh.sh
@@ -5,10 +5,12 @@ if [[ `uname -s` == 'Linux' ]]; then
     # ref: https://github.com/GuillaumeGomez/sysinfo/blob/43a12462624ce0c97105561775af059c5e4c0e35/src/unix/linux/system.rs#L389-L395
     case `(cat /etc/os-release 2>/dev/null || cat /etc/lsb-release 2>/dev/null) | grep ^ID=` in
     ID=arch)
-        echo "Environment : ?Unknown? + Unix Shell"
+        echo "Environment : Arch Linux + Unix Shell"
+        curl -L dotfiles.ranolp.dev/setup-scripts/linux/arch.sh | sh
         ;;
     *)
         echo "Environment : ?Unknown Linux? + Unix Shell"
+        ;;
     esac
 else
     echo "Environment : ?Unknown? + Unix Shell"

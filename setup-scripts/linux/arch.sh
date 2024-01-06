1ccd02e94d05712bfed966d8ae5ccf1ef113dea5
RanolP <me@ranolp.dev>
2024-01-06 22:59:40 +0900

    separate paru install

--- setup-scripts/linux/arch.sh
+++ setup-scripts/linux/arch.sh
@@ -7,13 +7,6 @@ sudo pacman -Sy --noconfirm git
 # nushell
 echo $ sudo pacman -Sy --noconfirm nushell
 sudo pacman -Sy --noconfirm nushell
-# paru
-echo $ # Install paru
-echo $ sudo pacman -Sy --noconfirm --needed base-devel
-sudo pacman -Sy --noconfirm --needed base-devel
-git clone https://aur.archlinux.org/paru.git
-(cd paru; makepkg -si)
-rm -rf paru
 
 # run common windows setup script
 nu -c "nu -c (http get https://dotfiles.ranolp.dev/setup-scripts/nu.nu)"

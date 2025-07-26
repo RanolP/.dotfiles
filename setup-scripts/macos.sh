80d7f63793754671dbd9308cdfeee1ce3fba626c
RanolP <me@ranolp.dev>
2025-07-26 19:20:28 +0900

    Add Homebrew installation check and essential package installations to macOS setup script

--- setup-scripts/macos.sh
+++ setup-scripts/macos.sh
@@ -2,4 +2,15 @@
 echo Installing essential packages...
 echo You may interfered with several popups.
 
-echo in fact there's no impl yet.
+if ! command -v brew >/dev/null; then
+    echo "There is no Homebrew yet; install now..."
+    # ref: https://brew.sh/
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+echo "$ brew install git"
+brew install git
+echo "$ brew install nushell"
+brew install nushell
+
+nu -c "nu -c (http get https://dotfiles.ranolp.dev/setup-scripts/nu.nu)"
